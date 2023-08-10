7c3adf9cb2b31b808a2f16b0f164e1b64f200fc0
kelley92 <kelley92@bu.edu>
2023-08-10 15:20:44 -0400

    uncommented code in NDC-RFtrainer. May need to edit path to NE_dstore.npy if not stored in working directory

--- NDC-RFtrainer.py
+++ NDC-RFtrainer.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.signal import welch
 
@@ -39,7 +39,8 @@ def get_simplified_features(x, wamp_threshold=0.01):
     return np.array([IEMG, MAV, SSI, RMS, VAR, WL, ZC, SSC, WAMP, FMD, FMM, CF, IASD, IATD, IEAV, IE])
 
 # Load the data
-matrix = np.load(r"C:\Users\Gabog\Downloads\NE_dstore.npy")
+# matrix = np.load(r"C:\Users\Gabog\Downloads\NE_dstore.npy")
+matrix = np.load("NE_dstore.npy")
 fs = 100
 go_data = matrix[matrix[:, -1] == 1, :-1]
 no_go_data = matrix[matrix[:, -1] == 0, :-1]
@@ -59,14 +60,14 @@ df = pd.DataFrame(all_features, columns=[
 df['label'] = all_labels
 
 # Generating the scatter plot matrix
-sns.pairplot(df, hue='label', diag_kind="kde", markers=["o", "s"], palette="muted")
-plt.show()
+# sns.pairplot(df, hue='label', diag_kind="kde", markers=["o", "s"], palette="muted")
+# plt.show()
 
 
 # import numpy as np
-# from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
-# from scipy.stats import skew, kurtosis
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from scipy.stats import skew, kurtosis
 
 # # Load the data
 # matrix = np.load(r"C:\Users\Gabog\Downloads\NE_dstore.npy")
@@ -130,81 +131,82 @@ plt.show()
 
 
 # # Extract basic features
-# def get_features(x):
-#     x = x - np.mean(x)
-#     abs_x = np.abs(x)
-#     len_x = len(x)
-#     IEMG = np.sum(abs_x)
-#     MAV = IEMG/len_x
-#     SSI = np.sum(np.square(x))
-#     RMS = np.sqrt((1/len_x)*SSI)
-#     VAR = np.var(x)
-#     #no MYOP bc idk wanna set threshold
-#     WL = np.sum(np.array([np.abs(x[i+1] - x[i]) for i in range(len_x-1)]))
-#     DAMV = (1/(len_x-1))*WL
-#     M2 = np.sum(np.array([np.square(x[i+1] - x[i]) for i in range(len_x-1)]))
-#     DVARV = (1/(len_x-2))*M2
-#     DASDV = np.sqrt((1/(len_x-1))*DVARV)
-#     MAX = np.max(x)
-#     MIN = np.min(x)
-#     IASD = np.sum(np.diff(abs_x, 2))
-#     IATD = np.sum(np.diff(abs_x, 3))
-#     IEAV = np.sum(np.exp(abs_x))
-#     IE = np.sum(np.exp(x))
-#     SKEW = skew(x)
-#     KURT = kurtosis(x, fisher=False)
+def get_features(x):
+    x = x - np.mean(x)
+    abs_x = np.abs(x)
+    len_x = len(x)
+    IEMG = np.sum(abs_x)
+    MAV = IEMG/len_x
+    SSI = np.sum(np.square(x))
+    RMS = np.sqrt((1/len_x)*SSI)
+    VAR = np.var(x)
+    #no MYOP bc idk wanna set threshold
+    WL = np.sum(np.array([np.abs(x[i+1] - x[i]) for i in range(len_x-1)]))
+    DAMV = (1/(len_x-1))*WL
+    M2 = np.sum(np.array([np.square(x[i+1] - x[i]) for i in range(len_x-1)]))
+    DVARV = (1/(len_x-2))*M2
+    DASDV = np.sqrt((1/(len_x-1))*DVARV)
+    MAX = np.max(x)
+    MIN = np.min(x)
+    IASD = np.sum(np.diff(abs_x, 2))
+    IATD = np.sum(np.diff(abs_x, 3))
+    IEAV = np.sum(np.exp(abs_x))
+    IE = np.sum(np.exp(x))
+    SKEW = skew(x)
+    KURT = kurtosis(x, fisher=False)
     
-#     ret = np.array((IEMG, MAV, SSI, RMS, VAR, WL, DAMV, M2, DVARV, DASDV, MAX, MIN, IASD, IATD, IEAV, IE, SKEW, KURT))
-#     return ret
+    ret = np.array((IEMG, MAV, SSI, RMS, VAR, WL, DAMV, M2, DVARV, DASDV, MAX, MIN, IASD, IATD, IEAV, IE, SKEW, KURT))
+    return ret
 
-# for i in range(len(matrix[:, 0])):
-#     matrix[i, :-1] = matrix[i, :-1] - np.mean(matrix[i, :-1])
+for i in range(len(matrix[:, 0])):
+    matrix[i, :-1] = matrix[i, :-1] - np.mean(matrix[i, :-1])
 
-# X = np.array([row[:-1] for row in matrix])
-# y = matrix[:, -1]
+X = np.array([get_features(row[:-1]) for row in matrix])
+y = matrix[:, -1]
 
-# # Split the data into training (80%) and testing (20%) sets with shuffling
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, shuffle=True)
+# Split the data into training (80%) and testing (20%) sets with shuffling
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42, shuffle=True)
 
-# # Initialize and train the Random Forest with default parameters on the training set
-# clf = RandomForestClassifier(random_state=42)
-# clf.fit(X_train, y_train)
+# Initialize and train the Random Forest with default parameters on the training set
+clf = RandomForestClassifier(random_state=42)
+clf.fit(X_train, y_train)
 
-# # Evaluate the model on both the training and test sets
-# train_predictions = clf.predict(X_train)
-# train_accuracy = np.mean(train_predictions == y_train)
+# Evaluate the model on both the training and test sets
+train_predictions = clf.predict(X_train)
+train_accuracy = np.mean(train_predictions == y_train)
 
-# test_predictions = clf.predict(X_test)
-# test_accuracy = np.mean(test_predictions == y_test)
+test_predictions = clf.predict(X_test)
+test_accuracy = np.mean(test_predictions == y_test)
 
-# print("Training Accuracy:", train_accuracy)
-# print("Test Accuracy:", test_accuracy)
+print("Training Accuracy:", train_accuracy)
+print("Test Accuracy:", test_accuracy)
 
 # import matplotlib.pyplot as plt
 
-# # Feature names for the basic features
-# feature_names = ['IEMG', 'MAV', 'SSI', 'RMS', 'VAR', 'WL', 'DAMV', 'M2', 'DVARV', 'DASDV', 'MAX', 'MIN', 'IASD', 'IATD', 'IEAV', 'IE', 'SKEW', 'KURT',
-#                     'ZCR', 'SSC', 'activity', 'mobility', 'complexity']
-# #['IEMG', 'MAV', 'SSI', 'RMS', 'VAR', 'WL', 'DAMV', 'M2', 'DVARV', 'DASDV', 'MAX', 'MIN', 'IASD', 'IATD', 'IEAV', 'IE']
+# Feature names for the basic features
+feature_names = [
+    'IEMG', 'MAV', 'SSI', 'RMS', 'VAR', 'WL', 'DAMV', 'M2', 'DVARV', 'DASDV', 'MAX', 'MIN', 'IASD', 'IATD', 'IEAV', 'IE', 'SKEW', 'KURT'
+]
+#['IEMG', 'MAV', 'SSI', 'RMS', 'VAR', 'WL', 'DAMV', 'M2', 'DVARV', 'DASDV', 'MAX', 'MIN', 'IASD', 'IATD', 'IEAV', 'IE']
 
-# # Extract feature importances from the trained Random Forest model
-# importances = clf.feature_importances_
+# Extract feature importances from the trained Random Forest model
+importances = clf.feature_importances_
 
-# # Pair the feature names with their importance values
-# feature_importance = list(zip(feature_names, importances))
+# Pair the feature names with their importance values
+feature_importance = list(zip(feature_names, importances))
 
-# # Sort features by importance
-# sorted_feature_importance = sorted(feature_importance, key=lambda x: x[1], reverse=True)
+# Sort features by importance
+sorted_feature_importance = sorted(feature_importance, key=lambda x: x[1], reverse=True)
 
-# # Separate the features and their importance values for visualization
-# features, importance_values = zip(*sorted_feature_importance)
+# Separate the features and their importance values for visualization
+features, importance_values = zip(*sorted_feature_importance)
 
-# # Visualize using a bar chart
-# plt.figure(figsize=(10,6))
-# plt.bar(features, importance_values, align='center')
-# plt.xlabel('Features')
-# plt.ylabel('Importance')
-# plt.title('Feature Importance using Random Forest')
-# plt.show()
+# Visualize using a bar chart
+plt.figure(figsize=(10,6))
+plt.bar(features, importance_values, align='center')
+plt.xlabel('Features')
+plt.ylabel('Importance')
+plt.title('Feature Importance using Random Forest')
+plt.show()
 
 
