2fb09d5c7199c7d30233b6c474afd5cc642579df
kelley92 <kelley92@bu.edu>
2023-10-31 16:31:07 -0400

    figures for NE and pupil at baseline and after stim for each outcome in each glm-hmm state

--- comparePhysAndPsych.m
+++ comparePhysAndPsych.m
@@ -3,36 +3,54 @@ animals = fetchAnimals(data);
 data(cellfun(@isempty, data.photometry_ch1),:) = [];
 ssd_version = 'v2';
 kstates = [2, 3, 4, 5, 6];
+% data_versions = {'last_trial_behavior_no_bias', ... 
+%     'spontaneous_mpfc_stim', ...
+%     'spontaneous_s1_stim', ...
+%     'spontaneous_pupil_stim'};
 data_versions = {'last_trial_behavior_no_bias', ... 
-    'spontaneous_mpfc_stim', ...
-    'spontaneous_s1_stim', ...
     'spontaneous_pupil_stim'};
 % data = rmDiscrepantTrials(data);
 animals_v1 = [3316, 3258, 3133, 200, 199, 198, 197, 196, 180, 167, 152];
 animals_v2 = [240, 241, 242, 243];
 animals = animals_v2;
 
-animal = 241;
-k = 5;
-fig = figure('Visible', 'on', 'WindowState', 'maximized');
-hold on;
+animal = 242;
+k = 3;
 
-for dv = 1:length(data_versions)
-    data_ver = data_versions{dv};
-    fformat = {data_ver, 'state_Python2mat.mat'};
-    results_dir = sprintf('NT-GLM-HMM/data/%s/%s/unshuffled/results/', ssd_version, data_ver);
-    tmp = filterTrials(data, 'animal', num2str(animal));
-    if startsWith(data_ver, 'behvaior') || startsWith(data_ver, 'last_trial')
-        tmp = removeFirstTrials(tmp);
-    end    
-    fname = sprintf('%s%i_%s_%i%s', results_dir, animal, fformat{1}, k, fformat{2});
-    subplot(length(data_versions), 4, (dv-1)*4+1)
-    hold on
-    plot_psycho_curves_states(fname, tmp, num2str(animal), k, data_ver);
-    xlabel('Stimulus Strength (PSI)', 'FontSize', 14, 'FontWeight', 'bold')
-    ylabel('Performance', 'FontSize', 14, 'FontWeight', 'bold')
-    plot_phys_by_states(fname, tmp, num2str(animal), k, length(data_versions), 4, (dv-1)*4+2)
-end
+% fig = figure('Visible', 'on', 'WindowState', 'maximized');
+% hold on;
+% for dv = 1:length(data_versions)
+%     data_ver = data_versions{dv};
+%     fformat = {data_ver, 'state_Python2mat.mat'};
+%     results_dir = sprintf('NT-GLM-HMM/data/%s/%s/unshuffled/results/', ssd_version, data_ver);
+%     tmp = filterTrials(data, 'animal', num2str(animal));
+%     if startsWith(data_ver, 'behvaior') || startsWith(data_ver, 'last_trial')
+%         tmp = removeFirstTrials(tmp);
+%     end    
+%     fname = sprintf('%s%i_%s_%i%s', results_dir, animal, fformat{1}, k, fformat{2});
+%     subplot(length(data_versions), 4, (dv-1)*4+1)
+%     hold on
+%     plot_psycho_curves_states(fname, tmp, num2str(animal), k, data_ver);
+%     xlabel('Stimulus Strength (PSI)', 'FontSize', 14, 'FontWeight', 'bold')
+%     ylabel('Performance', 'FontSize', 14, 'FontWeight', 'bold')
+%     plot_phys_by_states(fname, tmp, num2str(animal), k, length(data_versions), 4, (dv-1)*4+2)
+% end
+
+% for dv = 1:length(data_versions)
+%     data_ver = data_versions{dv};
+%     fformat = {data_ver, 'state_Python2mat.mat'};
+%     results_dir = sprintf('NT-GLM-HMM/data/%s/%s/unshuffled/results/', ssd_version, data_ver);
+%     tmp = filterTrials(data, 'animal', num2str(animal));
+%     if startsWith(data_ver, 'behvaior') || startsWith(data_ver, 'last_trial')
+%         tmp = removeFirstTrials(tmp);
+%     end
+%     fname = sprintf('%s%i_%s_%i%s', results_dir, animal, fformat{1}, k, fformat{2});
+%     % dilationByState(tmp, fname, k, data_ver)
+%     baselinePupilByState(tmp, fname, k, data_ver)
+% end
+baselinePupilByState(data, k, data_versions, animal)
+dilationByState(data, k, data_versions, animal)
+baselineNeByState(data, k, data_versions, animal)
 
 
 function plot_psycho_curves_states(filename, data, animal, k, data_version)
@@ -103,7 +121,7 @@ function plot_phys_by_states(filename, data, animal, k, nrows, ncols, si)
     results = load(filename);
     
     states = 0:k-1;
-    tbounds = [-0.5, 1.0];
+    tbounds = [-0.5, 6.0];
     cols = distinguishable_colors(length(states));
 
     for i = states
@@ -120,6 +138,7 @@ function plot_phys_by_states(filename, data, animal, k, nrows, ncols, si)
                 % keyboard
                 plot(t, ch1, 'DisplayName', sprintf('State %i (n=%i)', i, n))
             end
+            xlim([tbounds(1), tbounds(2)])
             subplot(nrows,ncols,si+1)
             hold on 
             try
@@ -128,7 +147,8 @@ function plot_phys_by_states(filename, data, animal, k, nrows, ncols, si)
                 % keyboard
                 plot(t, ch2, 'DisplayName', sprintf('State %i (n=%i)', i, n))
             end
-            [pupil, t] = avg_pupil_traces(tmp, [-0.6, 1.1]);
+            xlim([tbounds(1), tbounds(2)])
+            [pupil, t] = avg_pupil_traces(tmp, [tbounds(1)-0.1, tbounds(2)+0.1]);
             % keyboard
             subplot(nrows,ncols,si+2)
             hold on
@@ -138,6 +158,7 @@ function plot_phys_by_states(filename, data, animal, k, nrows, ncols, si)
                 % keyboard
                 plot(t, pupil, 'DisplayName', sprintf('State %i (n=%i)', i, n));
             end
+            xlim([tbounds(1), tbounds(2)])
             ch1_region = tmp.photometry_region_ch1{1,1};
             ch2_region = tmp.photometry_region_ch2{1,1};
         end
@@ -172,12 +193,22 @@ function plot_phys_by_states(filename, data, animal, k, nrows, ncols, si)
     % close
 end
 
-function [ch1mat, ch2mat, time] = avg_photo_traces(data, tbounds)
+function [ch1mat, ch2mat, time] = avg_photo_traces(data, tbounds, alignTo)
     % generates averages of photometry traces 
+    if ~exist('alignTo', 'var')
+        starts = data.stimulus_time;
+    elseif strcmp(alignTo, 'stimulus')
+        starts = data.stimulus_time;
+    elseif strcmp(alignTo, 'response')
+        starts = data.stimulus_time + data.response_time;
+        data(isnan(starts),:) = [];
+        starts = starts(~isnan(starts));
+    else
+        starts = data.stimulus_time;
+    end
     Fss = getFs(data, 'photometry_ch1');
     ch1mat = zeros(size(data,1), round(max(Fss)*diff(tbounds)));
     ch2mat = ch1mat;
-    starts = data.stimulus_time;
     time = linspace(tbounds(1), tbounds(2), round(max(Fss)*diff(tbounds)));
 
     for i = 1:size(data,1)
@@ -193,11 +224,21 @@ function [ch1mat, ch2mat, time] = avg_photo_traces(data, tbounds)
     end
 end
 
-function [pupil, time] = avg_pupil_traces(data, tbounds)
+function [pupil, time] = avg_pupil_traces(data, tbounds, alignTo)
     % generates averages of pupil traces
+    if ~exist('alignTo', 'var')
+        starts = data.stimulus_time;
+    elseif strcmp(alignTo, 'stimulus')
+        starts = data.stimulus_time;
+    elseif strcmp(alignTo, 'response')
+        starts = data.stimulus_time + data.response_time;
+        data(isnan(starts), :) = [];
+        starts = starts(~isnan(starts));
+    else
+        starts = data.stimulus_time;
+    end
     Fs = 1 / (data.pupil_area{1,1}(2,1)-data.pupil_area{1,1}(1,1));
     pupil = nan(size(data,1), round(Fs*diff(tbounds)));
-    starts = data.stimulus_time;
     time = linspace(tbounds(1), tbounds(2), round(Fs*diff(tbounds)));
 
     for i = 1:size(data,1)
@@ -210,4 +251,312 @@ function [pupil, time] = avg_pupil_traces(data, tbounds)
             pupil(i,:) = interp1(t,p,time);
         end
     end
+end
+
+function dilationByState(data, k, data_versions, animal)
+    data = filterTrials(data, 'animal', num2str(animal));
+    fig = figure();
+    tl = tiledlayout(length(data_versions),1);
+    axs = zeros(length(data_versions),1);
+    for i = 1:length(data_versions)
+        axs(i) = nexttile;
+    end
+    states = 0:k-1;
+    cols = distinguishable_colors(length(states));
+    for dv = 1:length(data_versions)
+        data_ver = data_versions{dv};
+        fformat = {data_ver, 'state_Python2mat.mat'};
+        results_dir = sprintf('NT-GLM-HMM/data/%s/%s/unshuffled/results/', 'v2', data_ver);
+        fname = sprintf('%s%i_%s_%i%s', results_dir, animal, fformat{1}, k, fformat{2});
+        if startsWith(data_ver, 'behvaior') || startsWith(data_ver, 'last_trial')
+           tmp = removeFirstTrials(data);
+        else
+           tmp = data;
+        end
+        results = load(fname);
+        offset = -0.2;
+        for i = states
+            stmp = tmp(results.predicted_states == i,:);
+            if ~isempty(stmp)
+                pupilDilationByOutcome(stmp, offset, axs(dv), cols(i+1,:))
+                offset = offset + 0.2;
+            end
+        end
+    end
+end
+
+function baselineNeByState(data, k, data_versions, animal)
+    data = filterTrials(data, 'animal', num2str(animal));
+    fig = figure();
+    tl = tiledlayout(length(data_versions),2);
+    axs = zeros(length(data_versions),2);
+    for i = 1:length(data_versions)
+        axs(i,1) = nexttile;
+        axs(i,2) = nexttile;
+    end
+    states = 0:k-1;
+    cols = distinguishable_colors(length(states));
+    for dv = 1:length(data_versions)
+        data_ver = data_versions{dv};
+        fformat = {data_ver, 'state_Python2mat.mat'};
+        results_dir = sprintf('NT-GLM-HMM/data/%s/%s/unshuffled/results/', 'v2', data_ver);
+        fname = sprintf('%s%i_%s_%i%s', results_dir, animal, fformat{1}, k, fformat{2});
+        if startsWith(data_ver, 'behvaior') || startsWith(data_ver, 'last_trial')
+           tmp = removeFirstTrials(data);
+        else
+           tmp = data;
+        end
+        results = load(fname);
+        offset = -0.2;
+        for i = states
+            stmp = tmp(results.predicted_states == i,:);
+            if ~isempty(stmp)
+                baselineNeByOutcome(stmp, offset, axs(dv,:), cols(i+1,:))
+                offset = offset + 0.2;
+            end
+        end
+    end
+end
+
+function baselinePupilByState(data, k, data_versions, animal)
+    data = filterTrials(data, 'animal', num2str(animal));
+    fig = figure();
+    tl = tiledlayout(length(data_versions),1);
+    axs = zeros(length(data_versions),1);
+    for i = 1:length(data_versions)
+        axs(i) = nexttile;
+    end
+    states = 0:k-1;
+    cols = distinguishable_colors(length(states));
+    for dv = 1:length(data_versions)
+        data_ver = data_versions{dv};
+        fformat = {data_ver, 'state_Python2mat.mat'};
+        results_dir = sprintf('NT-GLM-HMM/data/%s/%s/unshuffled/results/', 'v2', data_ver);
+        fname = sprintf('%s%i_%s_%i%s', results_dir, animal, fformat{1}, k, fformat{2});
+        if startsWith(data_ver, 'behvaior') || startsWith(data_ver, 'last_trial')
+           tmp = removeFirstTrials(data);
+        else
+           tmp = data;
+        end
+        results = load(fname);
+        offset = -0.2;
+        for i = states
+            stmp = tmp(results.predicted_states == i,:);
+            if ~isempty(stmp)
+                baselinePupilByOutcome(stmp, offset, axs(dv), cols(i+1,:))
+                offset = offset + 0.2;
+            end
+        end
+    end
+end
+
+function pupilDilationByOutcome(data, offset, ax, c)
+    % outcomes = unique(data.categorical_outcome);
+    outcomes = {'Hit', 'Miss', 'CR', 'FA'};
+    dilations = zeros(2,length(outcomes));
+    for o = 1:length(outcomes)
+        outcome = outcomes{o};
+        tmp = filterTrials(data, 'categorical_outcome', outcome);
+        if ~isempty(tmp)
+            [pupil, ~] = avg_pupil_traces(tmp, [0,6.0]);
+            [baseline, ~] = avg_pupil_traces(tmp, [-0.5,0.0], 'stimulus');
+            dilations(1,o) = nanmean(nanmean(pupil))-nanmean(nanmean(baseline));
+            dilations(2,o) = nanstd(nanmean(pupil,2)-nanmean(baseline,2)) / size(pupil,1);
+        else
+            dilations(1,o) = nan;
+            dilations(2,o) = nan;
+        end
+    end
+    axes(ax)
+    hold on
+    errorbar([1:size(dilations,2)]+offset, dilations(1,:), dilations(2,:), '.', 'Color', c)
+    bar([1:size(dilations,2)]+offset, dilations(1,:), 'FaceColor', c, 'BarWidth', 0.2)
+    action_outcomes = {{'Hit', 'FA'}, {'Miss', 'CR'}};
+    action_dilations = zeros(2,length(action_outcomes));
+    for ao = 1:length(action_outcomes)
+        outcome = action_outcomes{ao};
+        tmp = filterTrials(data, 'categorical_outcome', outcome);
+        [pupil, ~] = avg_pupil_traces(tmp, [0,6.0], 'stimulus');
+        [baseline, ~] = avg_pupil_traces(tmp, [-0.5,0.0], 'stimulus');
+        action_dilations(1,ao) = nanmean(nanmean(pupil))-nanmean(nanmean(baseline));
+        action_dilations(2,ao) = nanstd(nanmean(pupil,2)-nanmean(baseline,2)) / size(pupil,1);
+    end
+    x = size(dilations,2)+2:size(dilations,2)+1+size(action_dilations,2);
+    x = x + offset;
+    errorbar(x, action_dilations(1,:), action_dilations(2,:), '.', 'Color', c)
+    bar(x, action_dilations(1,:), 'FaceColor', c, 'BarWidth', 0.2)
+    xticks([1:size(dilations,2), x])
+    labels = [outcomes, {'Responded', 'Withheld'}];
+    xticklabels(labels)
+    ylabel('Mean Pupil Dilation (z-score)')
+end
+        
+
+function baselinePupilByOutcome(data, offset, ax, c)
+    outcomes = {'Hit', 'Miss', 'CR', 'FA'};
+    baselines = zeros(2,length(outcomes));
+    for o = 1:length(outcomes)
+        outcome = outcomes{o};
+        tmp = filterTrials(data, 'categorical_outcome', outcome);
+        if ~isempty(tmp)
+            [baseline, ~] = avg_pupil_traces(tmp, [-0.5,0.0], 'stimulus');
+            baselines(1,o) = nanmean(nanmean(baseline));
+            baselines(2,o) = nanstd(nanmean(baseline,2)) / size(baseline,1);
+        else
+            baselines(1,o) = nan;
+            baselines(2,o) = nan;
+        end
+    end
+    axes(ax)
+    hold on
+    errorbar([1:size(baselines,2)]+offset, baselines(1,:), baselines(2,:), '.', 'Color', c)
+    bar([1:size(baselines,2)]+offset, baselines(1,:), 'FaceColor', c, 'BarWidth', 0.2)
+    action_outcomes = {{'Hit', 'FA'}, {'Miss', 'CR'}};
+    action_baselines = zeros(2,length(action_outcomes));
+    for ao = 1:length(action_outcomes)
+        outcome = action_outcomes{ao};
+        tmp = filterTrials(data, 'categorical_outcome', outcome);
+        if ~isempty(tmp)
+            [baseline, ~] = avg_pupil_traces(tmp, [-0.5,0.0], 'stimulus');
+            action_baselines(1,ao) = nanmean(nanmean(baseline));
+            action_baselines(2,ao) = nanstd(nanmean(baseline,2)) / size(baseline,1);
+        else
+            action_baselines(1,ao) = nan;
+            action_baselines(2,ao) = nan;
+        end
+    end
+    x = size(baselines,2)+2:size(baselines,2)+1+size(action_baselines,2);
+    x = x + offset;
+    errorbar(x, action_baselines(1,:), action_baselines(2,:), '.', 'Color', c)
+    bar(x, action_baselines(1,:), 'FaceColor', c, 'BarWidth', 0.2)
+    xticks([1:size(baselines,2), x])
+    labels = [outcomes, {'Responded', 'Withheld'}];
+    xticklabels(labels)
+    ylabel('Mean Baseline Pupil Area (z-score)')
+end
+
+function baselineNeByOutcome(data, offset, ax, c)
+    outcomes = {'Hit', 'Miss', 'CR', 'FA'};
+    ch1b = zeros(2,length(outcomes));
+    ch2b = zeros(2,length(outcomes));
+    for o = 1:length(outcomes)
+        outcome = outcomes{o};
+        tmp = filterTrials(data, 'categorical_outcome', outcome);
+        if ~isempty(tmp)
+            [ch1, ch2, ~] = avg_photo_traces(tmp, [-0.5,0.0], 'stimulus');
+            ch1b(1,o) = nanmean(nanmean(ch1));
+            ch1b(2,o) = nanstd(nanmean(ch1,2)) / size(ch1,1);
+            ch2b(1,o) = nanmean(nanmean(ch2));
+            ch2b(2,o) = nanstd(nanmean(ch2,2)) / size(ch2,1);
+        else
+            ch1b(1,o) = nan;
+            ch1b(2,o) = nan;
+            ch2b(1,o) = nan;
+            ch2b(2,o) = nan;
+        end
+    end
+    axes(ax(1))
+    hold on
+    errorbar([1:size(ch1b,2)]+offset, ch1b(1,:), ch1b(2,:), '.', 'Color', c)
+    bar([1:size(ch1b,2)]+offset, ch1b(1,:), 'FaceColor', c, 'BarWidth', 0.2)
+    axes(ax(2))
+    hold on
+    errorbar([1:size(ch2b,2)]+offset, ch2b(1,:), ch2b(2,:), '.', 'Color', c)
+    bar([1:size(ch2b,2)]+offset, ch2b(1,:), 'FaceColor', c, 'BarWidth', 0.2)
+    % action trials
+    action_outcomes = {{'Hit', 'FA'}, {'Miss', 'CR'}};
+    action_ch1b = zeros(2,length(action_outcomes));
+    action_ch2b = zeros(2,length(action_outcomes));
+    for ao = 1:length(action_outcomes)
+        outcome = action_outcomes{ao};
+        tmp = filterTrials(data, 'categorical_outcome', outcome);
+        if ~isempty(tmp)
+            [ch1, ch2, ~] = avg_photo_traces(tmp, [-0.5,0.0], 'stimulus');
+            action_ch1b(1,ao) = nanmean(nanmean(ch1));
+            action_ch1b(2,ao) = nanstd(nanmean(ch1,2)) / size(ch1,1);
+            action_ch2b(1,ao) = nanmean(nanmean(ch2));
+            action_ch2b(2,ao) = nanstd(nanmean(ch2,2)) / size(ch2,1);
+        else
+            action_ch1b(1,ao) = nan;
+            action_ch1b(2,ao) = nan;
+            action_ch2b(1,ao) = nan;
+            action_ch2b(2,ao) = nan;
+        end
+    end
+    axes(ax(1))
+    x = size(ch1b,2)+2:size(ch1b,2)+1+size(action_ch1b,2);
+    x = x + offset;
+    errorbar(x, action_ch1b(1,:), action_ch1b(2,:), '.', 'Color', c)
+    bar(x, action_ch1b(1,:), 'FaceColor', c, 'BarWidth', 0.2)
+    xticks([1:size(ch1b,2), x])
+    labels = [outcomes, {'Responded', 'Withheld'}];
+    xticklabels(labels)
+    xtickangle(45)
+    ylabel('Mean Baseline mPFC NE (z-score)', 'FontSize', 14)
+    axes(ax(2))
+    x = size(ch2b,2)+2:size(ch2b,2)+1+size(action_ch2b,2);
+    x = x + offset;
+    errorbar(x, action_ch2b(1,:), action_ch2b(2,:), '.', 'Color', c)
+    bar(x, action_ch2b(1,:), 'FaceColor', c, 'BarWidth', 0.2)
+    xticks([1:size(ch2b,2), x])
+    labels = [outcomes, {'Responded', 'Withheld'}];
+    xticklabels(labels)
+    xtickangle(45)
+    ylabel('Mean Baseline S1 NE (z-score)', 'FontSize', 14)
+end    
+
+function increaseInNE(data)
+    outcomes = {'Hit', 'Miss', 'CR', 'FA'};
+    mpfc = zeros(2,length(outcomes));
+    s1 = mpfc;
+    for o = 1:length(outcomes)
+        outcome = outcomes{o};
+        tmp = filterTrials(data, 'categorical_outcome', outcome);
+        [ch1, ch2, ~] = avg_photo_traces(tmp, [0,4.0], 'stimulus');
+        [ch1b, ch2b, ~] = avg_photo_traces(tmp, [-0.5,0.0], 'stimulus');
+        mpfc(1,o) = nanmean(nanmean(ch1))-nanmean(nanmean(ch1b));
+        mpfc(2,o) = nanstd(nanmean(ch1,2)-nanmean(ch1,2)) / size(ch1,1);
+        s1(1,o) = nanmean(nanmean(ch2))-nanmean(nanmean(ch2b));
+        s1(2,o) = nanstd(nanmean(ch2,2)-nanmean(ch2b,2)) / size(ch2,1);
+    end
+    action_outcomes = {{'Hit', 'FA'}, {'Miss', 'CR'}};
+    action_mpfc = zeros(2,length(action_outcomes));
+    action_s1 = action_mpfc;
+    for ao = 1:length(action_outcomes)
+        outcome = action_outcomes{ao};
+        tmp = filterTrials(data, 'categorical_outcome', outcome);
+        [ch1, ch2, ~] = avg_photo_traces(tmp, [0,6.0], 'stimulus');
+        [ch1b, ch2b, ~] = avg_photo_traces(tmp, [-0.5,0.0], 'stimulus');
+        action_mpfc(1,ao) = nanmean(nanmean(ch1))-nanmean(nanmean(ch1b));
+        action_mpfc(2,ao) = nanstd(nanmean(ch1,2)-nanmean(ch1b,2)) / size(ch1,1);
+        action_s1(1,ao) = nanmean(nanmean(ch2))-nanmean(nanmean(ch2b));
+        action_s1(2,ao) = nanstd(nanmean(ch2,2)-nanmean(ch2b,2)) / size(ch2,1);
+    end
+    x = size(mpfc,2)+2:size(mpfc,2)+1+size(action_mpfc,2);
+    fig = figure();
+    fig.Position = [1308, 1301, 1405, 573];
+    tl = tiledlayout(1,2,'TileSpacing','Compact');
+    axs = zeros(1,2);
+    axs(1) = nexttile;
+    hold on
+    errorbar(1:size(mpfc,2), mpfc(1,:), mpfc(2,:), 'k.')
+    bar(1:size(mpfc,2), mpfc(1,:), 'k')
+    errorbar(x, action_mpfc(1,:), action_mpfc(2,:), 'k.')
+    bar(x, action_mpfc(1,:), 'k')
+    xticks([1:size(mpfc,2), x])
+    labels = [outcomes, {'Responded', 'Withheld'}];
+    xticklabels(labels)
+    xtickangle(45)
+    ylabel('Mean Evoked mPFC NE (z-score)', 'FontSize', 14)
+    axs(2) = nexttile;
+    hold on
+    errorbar(1:size(s1,2), s1(1,:), s1(2,:), 'k.')
+    bar(1:size(s1,2), s1(1,:), 'k')
+    errorbar(x, action_s1(1,:), action_s1(2,:), 'k.')
+    bar(x, action_s1(1,:), 'k')
+    xticks([1:size(s1,2), x])
+    labels = [outcomes, {'Responded', 'Withheld'}];
+    xticklabels(labels)
+    xtickangle(45)
+    ylabel('Mean Evoked S1 NE (z-score)', 'FontSize', 14)
 end
