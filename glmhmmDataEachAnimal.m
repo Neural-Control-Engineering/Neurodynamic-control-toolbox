3c5ab7c5355b65045711f6c42f3703ef98cf0397
kelley92 <kelley92@bu.edu>
2024-01-17 11:19:12 -0500

    more merge

--- glmhmmDataEachAnimal.m
+++ glmhmmDataEachAnimal.m
@@ -22,16 +22,11 @@ ssd_version = 'v3';
 % data_versions = {'dynamic_state'};
 % data_versions = {'spontaneous_mpfc_s1_stim'}
 % data_versions = {'spontaneous_pupil_stim_drop_outliers'};
-<<<<<<< HEAD
-data_versions = {'spontaneous_pupil_stim_1s_v2', ...
-                'spontaneous_pupil_stim_v2'};
-=======
 % data_versions = {'spontaneous_pupil_stim_1s_v2', ...
 %                 'spontaneous_pupil_stim_drop_outliers'};
 data_versions = {'spontaneous_pupil_stim_v2', ...
                 'last_trial_behavior_no_bias', ...
                 'last_trial_behavior_drop_stim_no_bias'};
->>>>>>> 5bf0027d217aa6e48b7335cd3de8a23fa06a50f1
 
 for i = 1:length(data_versions)
     data_version = data_versions{i};
