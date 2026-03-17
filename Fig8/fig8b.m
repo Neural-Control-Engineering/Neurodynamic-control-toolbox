f6e868d43cfe386c283a1d7b961234aeaf3a8a06
kelley92 <kelley92@bu.edu>
2026-03-17 09:49:09 -0400

    starting reviewer comments

--- Fig8/fig8b.m
+++ Fig8/fig8b.m
@@ -1,7 +1,7 @@
 function fig8b(data, k, data_ver, ssd_version, psychver, animals, shuff)
     corrs = lumpByResponseProb(data_ver, ssd_version, psychver, animals, data, k, shuff);
     corrs = [corrs{1}, corrs{2}, corrs{3}, corrs{4}];
-    [p, ~, stats] = anova1(corrs);
+    [p, ~, stats] = anova1(corrs)
     fprintf('Xcorr by state:\n')
     fprintf(sprintf('one way anova: p = %d\n', p))
     mc = multcompare(stats)
