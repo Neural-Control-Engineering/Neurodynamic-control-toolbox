ced2e2f3c89b109bc5a1d3edd03ca9014a1f8de3
kelley92 <kelley92@bu.edu>
2026-08-04 16:30:21 -0400

    supplemental figure 1 should use max rather than mean for pupil dilation

--- SupplementalFigs/suppFig1.m
+++ SupplementalFigs/suppFig1.m
@@ -35,7 +35,8 @@ function suppFig1(data, tbounds, alignTo)
         b = nanmean(pupil(:,(t > -0.5 & t < 0)), 2);
         % mean over the post-stimulus window, matching dilationsByOutcome.m and
         % increase{S1,Mpfc}NeByOutcome.m -- the convention the Methods describes
-        e = nanmean(pupil(:,(t > 0 & t < 6)), 2);
+        % e = nanmean(pupil(:,(t > 0 & t < 6)), 2);
+        e = max(pupil(:,(t > 0 & t < 6)), [], 2);
         d = e - b;
         baselines = [baselines; b];
         dilations = [dilations; d];
